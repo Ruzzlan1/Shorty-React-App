31dea7b3d5e6c4ba84692a0fae2eb5722aa27e34
Ruzzlan <voramzoram1@gmail.com>
2022-02-14 02:07:24 +0400

    Modifying App and index files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import Intro from './components/Intro'
+import Shortener from './components/Shortener'
+import About from './components/About'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+// import './style.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    <div className="container">
+      <Navbar />
+      <Intro />
+      <Shortener />
+      <About />
+      <Banner />
+      <Footer />
     </div>
-  );
+    // </div>
+  )
 }
 
-export default App;
+export default App
