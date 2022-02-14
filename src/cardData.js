a13a22704803096ac95f0b4316fff8a68c0aade2
Ruzzlan <voramzoram1@gmail.com>
2022-02-15 01:58:56 +0400

    New data added in cardData.js file

---
+++ src/cardData.js
@@ -0,0 +1,54 @@
+export const cardData = [
+  {
+    id: 1,
+    title: 'Brand Recognition',
+    description:
+      'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+    cardImg: 'salam',
+  },
+  {
+    id: 2,
+    title: ' Detailed Records',
+    description:
+      'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+    cardImg: 'icon-detailed-records.svg',
+  },
+  {
+    id: 3,
+    title: 'Fully Customizable',
+    description:
+      ' Improve brand awareness and content discoverability through customizable',
+    cardImg: 'icon-fully-customizable.svg',
+  },
+]
+
+export const footerData = [
+  {
+    id: 1,
+    title: '  Features',
+    categories: {
+      one: 'Link Shortening',
+      two: 'Branded Links',
+      three: 'Analytics',
+    },
+  },
+  {
+    id: 2,
+    title: 'Resources',
+    categories: {
+      one: 'Blog',
+      two: 'Developers',
+      three: 'Support',
+    },
+  },
+  {
+    id: 3,
+    title: 'Company',
+    categories: {
+      one: 'About',
+      two: 'Our Team',
+      three: 'Careers',
+      four: 'Contact',
+    },
+  },
+]
