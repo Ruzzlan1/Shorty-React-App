c8d2ac4bd771852a7f8873399013ec7bd4444c35
Ruzzlan <voramzoram1@gmail.com>
2022-03-10 21:06:05 +0400

    new helper function added for improvement

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-
+import { TIMEOUT_SEC } from './config'
 export const useOutsideAlerter = (ref, setter) => {
   useEffect(() => {
     //Alert if clicked on outside of element
@@ -27,4 +27,23 @@ export const findId = (id, component) => {
       return { ...item, clicked: true }
     })
   )
+}
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`))
+    }, s * 1000)
+  })
+}
+export const getJSON = async url => {
+  try {
+    const fetchPromise = await fetch(url)
+    const res = await Promise.race([fetchPromise, timeout(TIMEOUT_SEC)])
+    const data = await res.json()
+
+    return data
+  } catch (error) {
+    throw error
+  }
 }
